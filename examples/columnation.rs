e5277e0fde79bf8c15a02d9f8a1b3587400f8082
Frank McSherry <fmcsherry@me.com>
2021-03-12 15:47:23 -0500

    demonstrate potential improvement

--- examples/columnation.rs
+++ examples/columnation.rs
@@ -10,6 +10,15 @@ fn main() {
     let keys: u32 = std::env::args().nth(1).unwrap().parse().unwrap();
     let size: u32 = std::env::args().nth(2).unwrap().parse().unwrap();
 
+    let mode = std::env::args().any(|a| a == "new");
+
+    if mode {
+        println!("Running NEW arrangement");
+    }
+    else {
+        println!("Running OLD arrangement");
+    }
+
     // define a new computational scope, in which to run BFS
     timely::execute_from_args(std::env::args(), move |worker| {
 
@@ -17,36 +26,74 @@ fn main() {
 
         // define BFS dataflow; return handles to roots and edges inputs
         let mut probe = Handle::new();
-        let mut input = worker.dataflow(|scope| {
+        let (mut data_input, mut keys_input) = worker.dataflow(|scope| {
 
-            use timely::dataflow::operators::Probe;
             use differential_dataflow::operators::arrange::Arrange;
             use differential_dataflow::trace::implementations::ord::{OrdKeySpine, ColKeySpine};
 
-            let (keys_input, keys) = scope.new_collection();
-            // keys.arrange::<ColKeySpine<_,_,_>>()
-            keys.arrange::<OrdKeySpine<_,_,_>>()
-                .stream
-                .probe_with(&mut probe);
+            let (data_input, data) = scope.new_collection::<String, isize>();
+            let (keys_input, keys) = scope.new_collection::<String, isize>();
 
-            keys_input
+            if mode {
+                let data = data.arrange::<ColKeySpine<_,_,_>>();
+                let keys = keys.arrange::<ColKeySpine<_,_,_>>();
+
+                use differential_dataflow::operators::JoinCore;
+                keys.join_core(&data, |_k, &(), &()| Some(()))
+                    .probe_with(&mut probe);
+            }
+            else {
+                let data = data.arrange::<OrdKeySpine<_,_,_>>();
+                let keys = keys.arrange::<OrdKeySpine<_,_,_>>();
+
+                use differential_dataflow::operators::JoinCore;
+                keys.join_core(&data, |_k, &(), &()| Some(()))
+                    .probe_with(&mut probe);
+            }
+
+            (data_input, keys_input)
         });
 
+        // Load up data in batches.
         let mut counter = 0;
         while counter < 10 * keys {
             for i in 0 .. size {
                 let val = (counter + i) % keys;
-                let array: Vec<_> = (0 .. 1024).map(|_| val).collect();
-                input.insert(array);
+                data_input.insert(format!("{:?}", val));
             }
             counter += size;
-            input.advance_to(input.time() + 1);
-            input.flush();
-            while probe.less_than(input.time()) {
+            data_input.advance_to(data_input.time() + 1);
+            data_input.flush();
+            keys_input.advance_to(keys_input.time() + 1);
+            keys_input.flush();
+            while probe.less_than(data_input.time()) {
                 worker.step();
             }
         }
-        println!("{:?}\tcomplete", timer.elapsed());
+        println!("{:?}\tloading complete", timer.elapsed());
+
+        let mut queries = 0;
+
+        while queries < 10 * keys {
+            for i in 0 .. size {
+                let val = (queries + i) % keys;
+                keys_input.insert(format!("{:?}", val));
+            }
+            queries += size;
+            data_input.advance_to(data_input.time() + 1);
+            data_input.flush();
+            keys_input.advance_to(keys_input.time() + 1);
+            keys_input.flush();
+            while probe.less_than(data_input.time()) {
+                worker.step();
+            }
+        }
+
+        println!("{:?}\tqueries complete", timer.elapsed());
+
+        loop {
+
+        }
 
     }).unwrap();
 }
